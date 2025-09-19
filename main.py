3fde455dc2685bd26d9bc27e685ff119bba132be
sarveshash <136432405+sarveshash@users.noreply.github.com>
2025-09-19 10:48:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1351,6 +1351,7 @@ async def select_format(event):
     mode, fmt = (g.decode() for g in event.pattern_match.groups())
     await event.edit("__**Preparing battle requirements...**__") 
     await battle_create(user_id, mode, fmt)
+    battle_state[user_id] = {} 
     battle_state[int(user_id)]["mode"] = mode
     battle_state[int(user_id)]["fmt"] = fmt
     battle_state[int(user_id)]["team"] = battle_data[user_id]["team"] 
