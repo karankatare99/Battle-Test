4f77cd53326c18f14e3a5fa1b7f6d156e20cfd9e
sarveshash <136432405+sarveshash@users.noreply.github.com>
2025-10-12 17:46:32 +0530

    Update battle_handlers.py

--- handlers/battle_handlers.py
+++ handlers/battle_handlers.py
@@ -1726,13 +1726,13 @@ async def awaiting_move_action(room_id, fmt, move, poke, event):
     try:
         print("got out - processing turn")
         if room_id not in status_effects:
-            status_effects[roomid] = {}
+            status_effects[room_id] = {}
             # Define all possible conditions (status ailments)
             conditions = ["paralysis", "burn", "poison", "sleep", "confusion", "freeze","flinch"]
 
             # Initialize both players’ status lists
-            status_effects[roomid][user_id] = {cond: [] for cond in conditions}
-            status_effects[roomid][opponent_id] = {cond: [] for cond in conditions}
+            status_effects[room_id][p1_id] = {cond: [] for cond in conditions}
+            status_effects[room_id][p2_id] = {cond: [] for cond in conditions}
         # Get moves
         p1_move = selected_move[p1_id]["move"]
         p2_move = selected_move[p2_id]["move"]
