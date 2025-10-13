584d9a243c62265a7b89576166750d39ca611447
sarveshash <136432405+sarveshash@users.noreply.github.com>
2025-10-14 04:23:22 +0530

    Update battle_handlers.py

--- handlers/battle_handlers.py
+++ handlers/battle_handlers.py
@@ -1951,7 +1951,7 @@ async def awaiting_move_action(room_id, fmt, move, poke, event):
             userpoke=poke
             stats_modifier[room_id][p1_id][userpoke]={"atk":0,"def":0,"spa":0,"spd":0,"spe":0}
         if battle_state[p2_id]["active_pokemon"][0] not in stats_modifier[room_id][p2_id]:
-            opppoke=battle_state[opponent_id]["active_pokemon"][0]
+            opppoke=battle_state[p2_id]["active_pokemon"][0]
             stats_modifier[room_id][p2_id][userpoke]={"atk":0,"def":0,"spa":0,"spd":0,"spe":0}
         # Get moves
         p1_move = selected_move[p1_id]["move"]
